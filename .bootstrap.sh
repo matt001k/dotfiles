a886ace7637f5f735b4e51a34bab6d5d212ccef7
Matthew Krause <matthew.krause@dornerworks.com>
2024-02-15 10:49:03 -0500

    Adds fzf To .bootstrap.sh

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -51,3 +51,6 @@ echo "Updating Git"
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
 sudo apt-get install git -y
+
+echo "Installing FZF"
+sudo apt install fzf
