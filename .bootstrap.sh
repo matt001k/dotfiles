7e39752ba8bfa00908d0a4b1b8e9b6aa827983aa
Matthew Krause <kraus2mj@gmail.com>
2024-02-17 20:56:30 -0500

    Font Setup Precursor

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -18,6 +18,9 @@ echo "Installing Plugin Manager"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+echo "Setting Up Fonts"
+sudo apt install gnome-tweaks
+
 echo "Setting Up rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
